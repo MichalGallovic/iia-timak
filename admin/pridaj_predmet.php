ea26490304334a6f4a25573a5e804143d3c4a013
foxyq <foxyzv@gmail.com>
2014-12-17 18:11:20 +0000

    pridane php inserty v1

---
+++ admin/pridaj_predmet.php
@@ -0,0 +1,13 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'iia-timak/vendor/Mysql/MysqliDb.php';
+
+// nacitanie db credentails
+$credentials = include $_SERVER['DOCUMENT_ROOT'].'iia-timak/configs/database.php';
+$db = new MysqliDb ($credentials['host'], $credentials['username'],
+    $credentials['password'], $credentials['dbName']);
+
+    
+    var_dump($_POST);
+    
+    
+    ?>
