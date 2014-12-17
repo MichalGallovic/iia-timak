4a062f6d04f3b7141e48c834668ea85b3e6cb3ae
Michal Gallovic <michalgallovic@Michals-MacBook-Pro-2.local>
2014-12-17 20:37:58 +0100

    composer autoload added

--- server/howto/index.php
+++ server/howto/index.php
@@ -1,8 +1,7 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'iia-timak/vendor/Mysql/MysqliDb.php';
+require '../../vendor/autoload.php';
+$credentials = getDbCredentias();
 
-// nacitanie db credentails
-$credentials = include $_SERVER['DOCUMENT_ROOT'].'iia-timak/configs/database.php';
 $db = new MysqliDb ($credentials['host'], $credentials['username'],
     $credentials['password'], $credentials['dbName']);
 
