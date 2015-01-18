3be319818ebd2af802687ea55c3aea734d2153bd
Kubiskoi <hoblikj@gmail.com>
2015-01-18 20:10:23 +0000

    bootstrap na kazdom cruude

--- app/templates/admin/subjects/get_all_subjects.php
+++ app/templates/admin/subjects/get_all_subjects.php
@@ -10,13 +10,34 @@ $subjects = $db->get('subjects');
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="/style/bootstrap.min.css">
+
 </head>
 <body>
-	<h1>Subjects</h1>
-	<ul>
-		<?php foreach($subjects as $subjects): ?>
-			<li><?php echo $subjects['name']; echo ", "; echo $subjects['code']; ?></li>
-		<?php endforeach; ?>
-	</ul>
+	<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					
+					<h1>Subjects</h1>
+					<table class="table table-striped">
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Code</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach($subjects as $subjects): ?>
+								<tr><td><?php echo $subjects['name']; ?></td><td><?php echo $subjects['code']; ?></td></tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+
+						
+					
+				</div>
+			</div>
+		</div>
+	
 </body>
 </html>
--- app/templates/admin/users/create_user.php
+++ app/templates/admin/users/create_user.php
@@ -11,52 +11,95 @@ $groups = $db->get('groups');
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+            <link rel="stylesheet" type="text/css" href="/style/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/style/bootstrap-select.min.css">
+
 </head>
 <body>
-	<h1>Create user</h1>
-	 
-	 <form action="<?php echo $app->urlFor('admin.users.store'); ?>" method="POST">
-   
-	 <ul>         
-            <li>Role
+    <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                        <h1>Create user</h1>
+                         
+                         <form action="<?php echo $app->urlFor('admin.users.store'); ?>" method="POST">
+                       
+                         <!-- <ul>  -->        
+                               <!--  <li> -->
 
-                <select name="role_id" />
+                               <div class="form-group">
+                                  <label >Role</label>
 
-                <?php foreach($roles as $role): ?>
-                    <option value="<?php echo $role['id'] ?>">
-                    	<?php echo $role['name']; ?>
-                    </option>
-                <?php endforeach; ?>
-   
-                </select>
+                                    <select class='form-control' name="role_id" />
 
-             </li>
+                                    <?php foreach($roles as $role): ?>
+                                        <option value="<?php echo $role['id'] ?>">
+                                            <?php echo $role['name']; ?>
+                                        </option>
+                                    <?php endforeach; ?>
+                       
+                                    </select>
+                                </div>
 
-
-            <li>Google <input type="text" name="google" /></li>
-            <li>First name <input type="text" name="firstname" /></li>
-            <li>Surname <input type="text" name="surname" /></li>
-            <li>Title 1 <input type="text" name="title1" /></li>
-            <li>Title 2 <input type="text" name="title2" /></li>
-            <li>LDAP <input type="text" name="ldap" /></li>
-            
-            <li>
-                <select name="group_id" />
-
-                <?php foreach($groups as $group): ?>
-                    <option value="<?php echo $group['id'] ?>">
-                    	<?php echo $group['code']; echo ", "; echo $group['name'];  ?>
-                  </option>
-                <?php endforeach; ?>
-
-                </select>
-            </li>
+                                <!--  </li> -->
 
 
-    </ul>
-    
-     <input type="submit" value="Add" />
-    </form>
+                                <!-- <li>Google <input type="text" name="google" /></li>
+                                <li>First name <input type="text" name="firstname" /></li>
+                                <li>Surname <input type="text" name="surname" /></li>
+                                <li>Title 1 <input type="text" name="title1" /></li>
+                                <li>Title 2 <input type="text" name="title2" /></li>
+                                <li>LDAP <input type="text" name="ldap" /></li> -->
+
+                                <div class="form-group">
+                                   <label >Google</label>
+                                   <input type="text" class="form-control" name="google"/>
+                                 </div>
+                                 <div class="form-group">
+                                    <label >First name</label>
+                                    <input type="text" class="form-control" name="firstname"/>
+                                  </div>
+                                  <div class="form-group">
+                                     <label >Surname</label>
+                                     <input type="text" class="form-control" name="surname"/>
+                                   </div>
+                                   <div class="form-group">
+                                      <label >Title 1</label>
+                                      <input type="text" class="form-control" name="title1"/>
+                                    </div>
+
+                                    <div class="form-group">
+                                       <label >Title 2</label>
+                                       <input type="text" class="form-control" name="title2"/>
+                                     </div>
+                                     <div class="form-group">
+                                        <label >LDAP</label>
+                                        <input type="text" class="form-control" name="ldap"/>
+                                      </div>
+                                
+                                <!-- <li> -->
+                                    <select class='form-control' name="group_id" />
+
+                                    <?php foreach($groups as $group): ?>
+                                        <option value="<?php echo $group['id'] ?>">
+                                            <?php echo $group['code']; echo ", "; echo $group['name'];  ?>
+                                      </option>
+                                    <?php endforeach; ?>
+
+                                    </select>
+                                <!-- </li> -->
+
+
+                        </ul>
+                        
+                         <input class='btn btn-primary' type="submit" value="Add" />
+                        </form>
+                    
+                    
+                    
+                </div>
+            </div>
+        </div>
+	
 
 
 </body>
