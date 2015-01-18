023871c8fa5365bcde74e33eff5ccb2bc6bc27df
Igor Packo <foxyzv@gmail.com>
2015-01-18 21:35:33 +0000

    pridavame headre, languaaage support, dorabame joiny exercises a scasti lectures

--- app/templates/admin/exercises/get_all_exercises.php
+++ app/templates/admin/exercises/get_all_exercises.php
@@ -1,19 +1,64 @@
 <?php
+use IIA\Lang\Lang as Lang;
 $credentials = $app->config('db');
 $db = new MysqliDb($credentials['host'], $credentials['username'],
             $credentials['password'], $credentials['dbName']);
 $groups = $db->get('exercises');
 
+
+$dayNames = array(
+    
+    0=>'Monday', 
+    1=>'Tuesday', 
+    2=>'Wednesday', 
+    3=>'Thursday', 
+    4=>'Friday', 
+    5=>'Saturday', 
+    6=>'Sunday'
+ );
+
+$dni = array(
+    
+    0=>'Pondelok', 
+    1=>'Utorok', 
+    2=>'Streda', 
+    3=>'Štvrtok', 
+    4=>'Piatok', 
+    5=>'Sobota', 
+    6=>'Nedeľa'
+ );
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title><?php echo Lang::get('navbar_brand') ?></title>
 	<link rel="stylesheet" type="text/css" href="/style/bootstrap.min.css">
 
 </head>
 <body>
+
+<div class="nav navbar-default">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><?php echo Lang::get('navbar_brand') ?></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?php echo $app->urlFor('login') ?>"><?php echo Lang::get('navbar_logout') ?></a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container -->
+</div>
 	<div class="container">
 			<div class="row">
 				<div class="col-md-12">
@@ -43,7 +88,46 @@ $groups = $db->get('exercises');
 						</thead>
 						<tbody>
 							<?php foreach($groups as $group): ?>
-								<tr><td><?php  echo $group['subject_id']; ?></td><td><?php echo $group['user_id'];?></td><td><?php echo $group['room_id']; ?></td><td><?php echo $group['day'];?></td><td><?php echo $group['start_time']; ?> </td><td><?php echo $group['end_time']; ?></td></tr>
+
+								<?php
+									// join na subjects 
+									$db->join("subjects s", "e.subject_id=s.id", "LEFT");
+                                    $db->where("s.id", $group['subject_id']);
+
+                                    $exers = $db->get ("exercises e", null, "s.acronym, s.name");
+
+                                    $acro = $exers[0][ 'acronym'];
+                                    $name = $exers[0][ 'name'];
+                                    
+
+                                    // join na exercises
+                                    $db->join("users u", "e.user_id=u.id", "LEFT");
+                                    $db->where("u.id", $group['user_id']);
+
+                                    $usrs = $db->get ("exercises e", null, "u.title1, u.title2, u.firstname, u.surname");
+
+                                    $surname = $usrs[0]['surname'];
+                                    $firstname = $usrs[0][ 'firstname'];
+                                    $t1 = $usrs[0]['title1'];
+                                    $t2 = $usrs[0]['title2'];
+
+
+                                    // join na rooms
+                                    $db->join("rooms r", "e.room_id=r.id", "LEFT");
+                                    $db->where("r.id", $group['room_id']);
+
+                                    $rms = $db->get ("exercises e", null, "r.name");
+
+                                    $rname = $rms[0]['name'];
+                                   
+
+								?>
+
+
+								<tr><td><?php  echo $acro; echo ", "; echo $name ?></td>
+									<td><?php echo $t1; echo " "; echo $firstname; echo " "; echo $surname; echo " "; echo $t2; ?></td>
+									<td><?php echo $rname; ?></td><td><?php echo $dni[$group['day']]; ?></td>
+									<td><?php echo $group['start_time']; ?> </td><td><?php echo $group['end_time']; ?></td></tr>
 							<?php endforeach; ?>
 							
 						</tbody>
--- app/templates/admin/lectures/edit_lecture.php
+++ app/templates/admin/lectures/edit_lecture.php
@@ -1,4 +1,5 @@
 <?php
+use IIA\Lang\Lang as Lang;
 $credentials = $app->config('db');
 $db = new MysqliDb($credentials['host'], $credentials['username'],
             $credentials['password'], $credentials['dbName']);
@@ -13,12 +14,34 @@ $lectures = $db->get('lectures');
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title><?php echo Lang::get('navbar_brand') ?></title>
             <link rel="stylesheet" type="text/css" href="/style/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/style/bootstrap-select.min.css">
 
 </head>
 <body>
+
+<div class="nav navbar-default">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><?php echo Lang::get('navbar_brand') ?></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?php echo $app->urlFor('login') ?>"><?php echo Lang::get('navbar_logout') ?></a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container -->
+</div>
     <div class="container">
             <div class="row">
                 <div class="col-md-5">
@@ -29,7 +52,48 @@ $lectures = $db->get('lectures');
                                 <select class='form-control' name="id" />
 
                                 <?php foreach($lectures as $lecture): ?>
-                                    <option value="<?php echo $lecture['id'] ?>"><?php echo "subject id: "; echo $lecture['subject_id']; echo ", start time "; echo $lecture['start_time']; echo ", end time"; echo $lecture['end_time']; echo ", room "; echo $lecture['room_id'];  echo ", "; echo $lecture['user_id'];  ?></option>
+
+
+                                <?php
+
+                                    // join na subjects
+                                    $db->join("subjects s", "l.subject_id=s.id", "LEFT");
+                                    $db->where("s.id", $lecture['subject_id']);
+
+                                    $exers = $db->get ("lectures l", null, "s.acronym, s.name");
+
+                                    $acro = $exers[0][ 'acronym'];
+                                    $name = $exers[0][ 'name'];
+                                    
+
+                                    // join na users
+                                    $db->join("users u", "l.user_id=u.id", "LEFT");
+                                    $db->where("u.id", $lecture['user_id']);
+
+                                    $usrs = $db->get ("lectures l", null, "u.title1, u.title2, u.firstname, u.surname");
+
+                                    $surname = $usrs[0]['surname'];
+                                    $firstname = $usrs[0][ 'firstname'];
+                                    $t1 = $usrs[0]['title1'];
+                                    $t2 = $usrs[0]['title2'];
+
+
+                                    // join na rooms
+                                    $db->join("rooms r", "l.room_id=r.id", "LEFT");
+                                    $db->where("r.id", $lecture['room_id']);
+
+                                    $rms = $db->get ("lectures l", null, "r.name");
+
+                                    $rname = $rms[0]['name'];
+
+                                   ?>
+
+                                    <option value="<?php echo $lecture['id'] ?>">
+                                        <?php echo $acro; echo ", "; echo $name; echo ", ";
+                                         echo $lecture['start_time']; echo "-"; echo $lecture['end_time'];
+                                          echo ", "; echo $rname;  echo ", "; 
+                                        echo $t1; echo " "; echo $firstname; echo " "; echo $surname; echo " "; echo $t2; ?>
+                                    </option>
                                 <?php endforeach; ?>
 
                                 </select>
@@ -102,13 +166,13 @@ $lectures = $db->get('lectures');
                                 <select class='form-control' name="day" />
 
                                 
-                                    <option value="0">Mon</option>
-                                    <option value="1">Tue</option>
-                                    <option value="2">Wed</option>
-                                    <option value="3">Thu</option>
-                                    <option value="4">Fri</option>
-                                    <option value="5">Sat</option>
-                                    <option value="6">Sun</option>
+                                    <option value="0">Monday</option>
+                                    <option value="1">Tuesday</option>
+                                    <option value="2">Wednesday</option>
+                                    <option value="3">Thursday</option>
+                                    <option value="4">Friday</option>
+                                    <option value="5">Saturday</option>
+                                    <option value="6">Sunday</option>
                                
 
                                 </select>
