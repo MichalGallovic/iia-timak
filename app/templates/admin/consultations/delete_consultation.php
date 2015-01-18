e7c87587985001ab1c8a6e3141e6a470d2729bf6
Igor Packo <foxyzv@gmail.com>
2015-01-18 18:07:50 +0000

    prerobene roles,rooms, consultations na hotovo (Kubove bootsrapy, moje joiny)

--- app/templates/admin/consultations/delete_consultation.php
+++ app/templates/admin/consultations/delete_consultation.php
@@ -4,6 +4,27 @@ $db = new MysqliDb($credentials['host'], $credentials['username'],
             $credentials['password'], $credentials['dbName']);
 $consultations = $db->get('consultations');
 
+$dayNames = array(
+    
+    0=>'Monday', 
+    1=>'Tuesday', 
+    2=>'Wednesday', 
+    3=>'Thursday', 
+    4=>'Friday', 
+    5=>'Saturday', 
+    6=>'Sunday'
+ );
+
+$dni = array(
+    
+    0=>'Pondelok', 
+    1=>'Utorok', 
+    2=>'Streda', 
+    3=>'Štvrtok', 
+    4=>'Piatok', 
+    5=>'Sobota', 
+    6=>'Nedeľa'
+ );
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,15 +40,57 @@ $consultations = $db->get('consultations');
 			<div class="col-md-4">
 				<h1>Delete Consultation</h1>
 					<form action="<?php echo $app->urlFor('admin.consultations.remove') ?>" method="POST">
+					
+
+
+
 					<select class="form-control" value="id" name="id">
 						<?php foreach($consultations as $consultation): ?>
+
+
+
+
+                                 <?php
+                                    
+                                    $db->join("subjects s", "c.subject_id=s.id", "LEFT");
+                                    $db->where("s.id", $consultation['subject_id']);
+
+                                    $subs = $db->get ("consultations c", null, "s.acronym, s.name");
+                                  // var_dump($subs[0]);
+
+                                    $sub= $subs[0][ 'name'];
+                                    $acro = $subs[0]['acronym'];
+
+
+                                    $db->join("users u", "c.user_id=u.id", "LEFT");
+                                    $db->where("u.id", $consultation['user_id']);
+
+                                    $usrs = $db->get ("consultations c", null, "u.title1, u.firstname, u.surname, u.title2");
+
+                                    $surname = $usrs[0][ 'surname'];
+                                    $firstname = $usrs[0][ 'firstname'];
+                                    $t1 = $usrs[0][ 'title1'];
+                                    $t2 = $usrs[0][ 'title2'];
+                                    
+                            
+                                    ?>   
+
+
+
+
 							<option name="<?php echo $consultation['id'] ?>" value="<?php echo $consultation['id'] ?>" >
-				<?php echo "day: ";echo $consultation['day']; echo ", note: "; echo $consultation['note'];
-							echo ", start "; echo $consultation['start_time'];echo ", "; echo $consultation['end_time']; ?>
-							 </option>
+								
+								<?php echo $dayNames[$consultation['day']];
+                                    echo ", "; echo $consultation['start_time']; echo "-"; echo $consultation['end_time']; 
+                                    echo ", "; echo $acro; echo ", "; echo $t1; echo " "; echo $firstname; echo " "; echo $surname;
+                                    echo ", "; echo $t2;  ?>
+
+							</option>
+
 						<?php endforeach; ?>
 
-					</select>
+ 					</select>
+
 					<div>
 					<input class="btn btn-primary" type="submit" value="delete" />
 					</div>
--- app/templates/admin/consultations/edit_consultation.php
+++ app/templates/admin/consultations/edit_consultation.php
@@ -7,6 +7,29 @@ $consultations = $db->get('consultations');
 $subjects = $db->get('subjects');
 $users = $db->get('users');
 $rooms = $db->get('rooms');
+
+$dayNames = array(
+    
+    0=>'Monday', 
+    1=>'Tuesday', 
+    2=>'Wednesday', 
+    3=>'Thursday', 
+    4=>'Friday', 
+    5=>'Saturday', 
+    6=>'Sunday'
+ );
+
+$dni = array(
+    
+    0=>'Pondelok', 
+    1=>'Utorok', 
+    2=>'Streda', 
+    3=>'Štvrtok', 
+    4=>'Piatok', 
+    5=>'Sobota', 
+    6=>'Nedeľa'
+ );
+
 ?>
 
 <!DOCTYPE html>
@@ -24,14 +47,42 @@ $rooms = $db->get('rooms');
                     <h1>Edit Consultation</h1> 
                     <form action="<?php echo $app->urlFor('admin.consultations.update'); ?>" method="POST">
                         
-
-
+                           
                                 <select class='form-control' name="id" />
 
                                 <?php foreach($consultations as $consultation): ?>
-                                    <option value="<?php echo $consultation['id'] ?>"><?php echo "day: "; echo $consultation['day'];
-                                    echo ", note "; echo $consultation['note']; echo ", start - end "; echo $consultation['start_time'];
-                                    echo "-"; echo $consultation['end_time']; ?></option>
+
+                                   
+                                      
+                                 <?php
+                                    
+                                    $db->join("subjects s", "c.subject_id=s.id", "LEFT");
+                                    $db->where("s.id", $consultation['subject_id']);
+
+                                    $subs = $db->get ("consultations c", null, "s.acronym, s.name");
+                                  // var_dump($subs[0]);
+
+                                    $sub= $subs[0][ 'name'];
+                                    $acro = $subs[0]['acronym'];
+
+
+                                    $db->join("users u", "c.user_id=u.id", "LEFT");
+                                    $db->where("u.id", $consultation['user_id']);
+
+                                    $usrs = $db->get ("consultations c", null, "u.title1, u.firstname, u.surname, u.title2");
+
+                                    $surname = $usrs[0][ 'surname'];
+                                    $firstname = $usrs[0][ 'firstname'];
+                                    $t1 = $usrs[0][ 'title1'];
+                                    $t2 = $usrs[0][ 'title2'];
+                                    
+                            
+                                    ?>         
+
+                                    <option value="<?php echo $consultation['id'] ?>"><?php echo $dayNames[$consultation['day']];
+                                    echo ", "; echo $consultation['start_time']; echo "-"; echo $consultation['end_time']; 
+                                    echo ", "; echo $acro; echo ", "; echo $t1; echo " "; echo $firstname; echo " "; echo $surname;
+                                    echo ", "; echo $t2;  ?></option>
                                 <?php endforeach; ?>
 
                                 </select>
