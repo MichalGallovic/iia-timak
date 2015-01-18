cca3db8e2383be0aba3bf14bfa98f9f419b9e75c
Igor Packo <foxyzv@gmail.com>
2015-01-18 22:36:53 +0000

    bootstrapy na celom crude, vsade joiny, chyba language na pevnych textoch + horne menu

--- app/templates/admin/groups/get_all_groups.php
+++ app/templates/admin/groups/get_all_groups.php
@@ -1,4 +1,6 @@
 <?php
+use IIA\Lang\Lang as Lang;
+
 $credentials = $app->config('db');
 $db = new MysqliDb($credentials['host'], $credentials['username'],
             $credentials['password'], $credentials['dbName']);
@@ -9,11 +11,32 @@ $groups = $db->get('groups');
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	 <title><?php echo Lang::get('navbar_brand') ?></title>
 	<link rel="stylesheet" type="text/css" href="/style/bootstrap.min.css">
 
 </head>
 <body>
+	<div class="nav navbar-default">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><?php echo Lang::get('navbar_brand') ?></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?php echo $app->urlFor('login') ?>"><?php echo Lang::get('navbar_logout') ?></a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container -->
+</div>
 	<div class="container">
 			<div class="row">
 				<div class="col-md-12">
