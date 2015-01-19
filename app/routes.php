894cab73eefc3ec15b23566862347b02b33d8568
Michal Gallovic <michalgallovic@Michals-MacBook-Pro-2.local>
2015-01-19 01:59:54 +0100

    fix

--- app/routes.php
+++ app/routes.php
@@ -36,7 +36,7 @@ $isLoggedIn = function() use ($app) {
 };
 
 $app->get('/service/:segments+', function($segments) use ($app) {
-    $API = new \IIA\service\MyAPI($segments, $app->config('db'));
+    $API = new IIA\service\MyAPI($segments, $app->config('db'));
     $responseData = $API->processAPI();
     $response = $app->response();
     $response['Content-Type'] = 'application/json';
