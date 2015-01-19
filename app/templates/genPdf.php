55c619a20daf6f39d6c7079c3cd75e903e458ca4
JakubFornadel <jakub.fornadel@gmail.com>
2015-01-19 07:46:03 +0000

    pomoc

--- app/templates/genPdf.php
+++ app/templates/genPdf.php
@@ -1,9 +1,7 @@
 <?php
 
-
-use IIA\PdfGen\InvoicePdf as InvoicePdf;
-//use \TCPDF as TCPDF;
-
+use \TCPDF as TCPDF;
+use IIA\service\MyApi as MyApi;
 
 date_default_timezone_set('Europe/Bratislava');
 error_reporting(E_ALL);
@@ -17,13 +15,13 @@ function generateHTML($data) {
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
         <head>
-            <title>Invoice</title>
+            <title>Schedule</title>
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
             <meta http-equiv="Content-language" content="en" />
             <meta name="keywords" content="PHP, sample, invoice, PDF, TCPDF"/>
             <meta name="description" content="A simple invoice example for 'Creating PDFs on the fly with TCPDF' on IBM's developerWorks"/>
             <style type="text/css" media="all">
-                @import url("Invoice.css");
+                @import url("Schedule.css");
             </style>
         </head>
         <body>
@@ -83,44 +81,148 @@ function generateHTML($data) {
     <?php
 }
 
+function getScheduleData() {
+    
+    $scheduleData = array(
+        'user' => 'user@stuba.sk',
+        'date' => date_format(new DateTime(), DateTime::W3C),
+        'items' => array()
+    );
+    $scheduleData['items'][] = array('Procesor Intel Core i5', 2, 109.99, 2 * 109.99);
+    $scheduleData['items'][] = array('Licencia Visual Studio 2010 Professional', 2, 29.35, 2 * 29.35);
+    $scheduleData['items'][] = array('MSDNAA licencia na produkty Microsoft', 12, 0, 24 * 0);
+    $scheduleData['total'] = 0;
+    foreach ($scheduleData['items'] as $item) {
+        $scheduleData['total'] += $item[3];
+    }
+    return $scheduleData;
+}
 
 //require_once( $docRoot . '../tcpdf/htmlcolors.php' );
 
+class SchedulePdf extends TCPDF {
+
+    private $scheduleData;
+
+    function __construct($data, $orientation, $unit, $format) {
+        parent::__construct($orientation, $unit, $format, true, 'UTF-8', false);
+
+        $this->scheduleData = $data;
+
+        # Set the page margins: 72pt on each side, 36pt on top/bottom.
+        $this->SetMargins(72, 36, 72, true);
+        $this->SetAutoPageBreak(true, 36);
+
+        # Set document meta-information
+        $this->SetCreator(PDF_CREATOR);
+        $this->SetAuthor('STU User (user@stuba.sk)');
+        $this->SetTitle('Schedule for ' . $this->scheduleData['user']);
+        $this->SetSubject("A simple schedule example for 'Creating PDFs on the fly with TCPDF' on IBM's developerWorks");
+        $this->SetKeywords('PHP, sample, schedule, PDF, TCPDF');
+
+        //set image scale factor
+        $this->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        //set some language-dependent strings
+        global $l;
+        $this->setLanguageArray($l);
+    }
+
+    # Page header and footer code.
+
+    public function Header() {
+        global $webcolor;
+
+        # The image is this much larger than the company name text.
+        $bigFont = 14;
+        $imageScale = ( 128.0 / 26.0 ) * $bigFont;
+        $smallFont = ( 16.0 / 26.0 ) * $bigFont;
+
+        //$this->ImagePngAlpha('logoFEI.png', 82, 36, 128, 128, $imageScale, $imageScale, 'PNG', null, 'T', false, 72, 'L');
+        $this->SetFont('times', 'b', $bigFont);
+        $this->Cell(0, 0, 'FEI STU', 0, 1);
+        $this->SetFont('times', 'i', $smallFont);
+        $this->Cell($imageScale);
+        $this->Cell(0, 0, '', 0, 1);
+        $this->Cell($imageScale);
+        $this->Cell(0, 0, 'Ilkovicova c.3, Mlynska dolina', 0, 1);
+        $this->Cell($imageScale);
+        $this->Cell(0, 0, 'Bratislava, Slovensko', 0, 1);
+
+        $this->SetY(1.5 * 72, true);
+        $this->SetLineStyle(array('width' => 2, 'color' => array($webcolor['black'])));
+        $this->Line(72, 36 + $imageScale, $this->getPageWidth() - 72, 36 + $imageScale);
+    }
+
+    public function Footer() {
+        global $webcolor;
+
+        $this->SetLineStyle(array('width' => 2, 'color' => array($webcolor['black'])));
+        $this->Line(72, $this->getPageHeight() - 1.5 * 72 - 2, $this->getPageWidth() - 72, $this->getPageHeight() - 1.5 * 72 - 2);
+        $this->SetFont('times', '', 8);
+        $this->SetY(-1.5 * 72, true);
+        $this->Cell(72, 0, 'Faktúra pripravená pre ' . $this->scheduleData['user'] . ' ' . $this->scheduleData['date']);
+    }
+
+    public function CreateSchedule() {
+        $this->AddPage();
+        $this->SetFont('helvetica', '', 11);
+        $this->SetY(144, true);
+
+        # Table parameters
+        #
+		# Column size, wide (description) column, table indent, row height.
+        $col = 72;
+        $wideCol = 3 * $col;
+        $indent = ( $this->getPageWidth() - 2 * 72 - $wideCol - 3 * $col ) / 2;
+        $line = 18;
+
+        # Table header
+        $this->SetFont('', 'b');
+        $this->Cell($indent);
+        $this->Cell($wideCol, $line, 'Položka', 1, 0, 'L');
+        $this->Cell($col, $line, 'Množstvo', 1, 0, 'R');
+        $this->Cell($col, $line, 'Cena ks', 1, 0, 'R');
+        $this->Cell($col, $line, 'Cena', 1, 0, 'R');
+        $this->Ln();
+
+        # Table content rows
+        $this->SetFont('', '');
+        foreach ($this->scheduleData['items'] as $item) {
+            $this->Cell($indent);
+            $this->Cell($wideCol, $line, $item[0], 1, 0, 'L');
+            $this->Cell($col, $line, $item[1], 1, 0, 'R');
+            $this->Cell($col, $line, $item[2], 1, 0, 'R');
+            $this->Cell($col, $line, $item[3], 1, 0, 'R');
+            $this->Ln();
+        }
+
+        # Table Total row
+        $this->SetFont('', 'b');
+        $this->Cell($indent);
+        $this->Cell($wideCol + $col * 2, $line, 'Cena dokopy:', 1, 0, 'R');
+        $this->SetFont('', '');
+        $this->Cell($col, $line, $this->scheduleData['total'], 1, 0, 'R');
+    }
+
+}
 
 function generatePDF($data) {
     # Create a new PDF document.
-    $pdf = new InvoicePdf($data, 'P', 'pt', 'LETTER');
+    $pdf = new SchedulePdf($data, 'P', 'pt', 'LETTER');
 
-    # Generate the invoice.
-    $pdf->CreateInvoice();
+    # Generate the schedule.
+    $pdf->CreateSchedule();
 
     # Output the PDF document.
     $pdf->Output('Your_Invoice.pdf', 'D');
 }
 
-# Set up the data.
-#
-# You could pull this data from a database, web service, XML
-# file, etc. To simplify the example, I'm just going to
-# include it directly here.
-$invoiceData = array(
-    'user' => 'user@stuba.sk',
-    'date' => date_format(new \DateTime(), \DateTime::W3C),
-    'items' => array()
-);
-$invoiceData['items'][] = array('Procesor Intel Core i5', 2, 109.99, 2 * 109.99);
-$invoiceData['items'][] = array('Licencia Visual Studio 2010 Professional', 2, 29.35, 2 * 29.35);
-$invoiceData['items'][] = array('MSDNAA licencia na produkty Microsoft', 12, 0, 24 * 0);
-$invoiceData['total'] = 0;
-foreach ($invoiceData['items'] as $item) {
-    $invoiceData['total'] += $item[3];
-}
-
 # If URL has ?PDF=1, we need to make a PDF instead of HTML.
 
 if (array_key_exists('PDF', $_REQUEST)) {
-    generatePDF($invoiceData);
+    generatePDF(getScheduleData());
 } else {
-    generateHTML($invoiceData);
+    generateHTML(getScheduleData());
 }
 ?>
